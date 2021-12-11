a75344278d33f80cfb2c3bdcad0481ed80a1da88
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-12-11 11:43:25 +0100

    Initial Commit

---
+++ MainAppMobileChallenge/MainAppMobileChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainAppMobileChallengeApp.swift
+//  MainAppMobileChallenge
+//
+//  Created by Michele Manniello on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MainAppMobileChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
