21789feca294152fcc5a73ed954a0990dcbb215e
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-12-14 17:13:53 +0100

    primo commit

--- MainAppMobileChallenge/ContentView.swift
+++ MainAppMobileChallenge/ContentView.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+import IosMobileChallenge
 
 struct ContentView: View {
+    @StateObject var user = UserList()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView{
+            HStack {
+                VStack(alignment: .leading) {
+                    if user.error != ""{
+                        Text("Error \(user.error)")
+                            .padding()
+                    }else{
+                        Text("Succes \(user.value)")
+                    }
+                }
+                Spacer()
+            }
+        }
+        .onAppear {
+            user.GetUser(owner: "octocat", name: "hello-world")
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
